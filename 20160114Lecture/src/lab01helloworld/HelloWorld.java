7b57f9c40a666e91330ef6f2342866d7646f5349
Vasco Horta <diasfonseca@live.com>
2016-01-18 19:26:31 +0000

    Adding comments
    Adding comments to the package and methods.

--- 20160114Lecture/src/lab01helloworld/HelloWorld.java
+++ 20160114Lecture/src/lab01helloworld/HelloWorld.java
@@ -1,7 +1,18 @@
 package lab01helloworld;
 
+/**
+ * The "Hello World" application printing the "Hello World"
+ * 
+ * @author Vasco
+ *
+ */
 public class HelloWorld {
 
+	/**
+	 * The Main method.
+	 * 
+	 * @param args
+	 */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
--- 20160114Lecture/src/lab01helloworld/HelloWorldWithCommandLineArguments.java
+++ 20160114Lecture/src/lab01helloworld/HelloWorldWithCommandLineArguments.java
@@ -1,7 +1,19 @@
 package lab01helloworld;
 
+/**
+ * The "Hello World" main accepting command line arguments
+ * for a dynamic messaging.
+ * 
+ * @author Vasco
+ *
+ */
 public class HelloWorldWithCommandLineArguments {
 
+	/**
+	 * Main method.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		if (args.length > 0) {
 			System.out.println(args[0]);
