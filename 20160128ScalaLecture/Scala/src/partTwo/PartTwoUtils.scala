99b60cc24f7f3da2fb563382c73328c835c2d245
Vasco Horta <diasfonseca@live.com>
2016-01-30 15:59:41 +0000

    PartTwo Q1 to 10

---
+++ 20160128ScalaLecture/Scala/src/partTwo/PartTwoUtils.scala
@@ -0,0 +1,14 @@
+package partTwo
+
+class PartTwoUtils {
+  // q7
+  def calc(num : Double) : Double = { num * num }
+  // q7
+  def cube(num : Double) : Double = { calc(num) * num }
+
+  // q8
+  def calc2(num : Int) : Int = { num * num }
+  def calc2(num : Double) : Double = { num * num }
+
+  
+}
---
+++ 20160128ScalaLecture/Scala/src/partTwo/exercisesPartTwo.scala
@@ -0,0 +1,88 @@
+package partTwo
+
+// Integer, Boolean, Long, Double
+object q1 {
+  var list = List(42, true, 123L, 42.0);
+  list foreach( (x) =>
+    println("The type of '"+x+"' is: "+x.getClass())
+  )
+}
+
+// Character and String
+object q2 {
+  var list = List('a', "a");
+  list foreach( (x) =>
+    println("The type of '"+x+"' is: "+x.getClass())
+  )
+}
+  
+// String and Unit
+object q3 {
+  var list = List("Hello world", println("Hello world"));
+  list foreach( (x) =>
+    println("The type of '"+x+"' is: "+x.getClass())
+  )
+}
+
+object q4 {
+  // Single quotes are for characters, not strings
+//  var list = List('Hello world');
+}
+
+object q5 {
+  // Predator
+  val a = 1
+  val b = 2
+  if (a>b) println("Alien") else println("Predator")
+
+  // 2001
+  if (a>b) println("Alien") else println(2001)
+  
+  // hello
+  if (true) println("hello")
+}
+
+object q6 {
+  // 1 + 2 + 3 has three integers that are being added and
+  // result in final integers 6
+  // 6 is one single integer
+}
+
+object q7 {
+  var utils = new PartTwoUtils
+  utils.calc(2)
+}
+
+object q8 {
+  var utils = new PartTwoUtils
+  utils.calc2(2)
+  utils.calc2(.32)
+}
+
+object q9 {
+  argh.c + argh.b + argh.a
+}
+object argh {
+  // a
+  def a = {
+    println("a")
+    1
+  }
+  // b a
+  def b = {
+    println("b")
+    a + 2
+  }
+  // c a b a
+  def c = {
+    println("c")
+    a
+    b + "c"
+  }
+}
+
+object exercises {
+  def main(args: Array[String]) : Unit = {
+    q1; q2; q3; q4; q5; q6; q7; q8; q9;
+  }
+}
