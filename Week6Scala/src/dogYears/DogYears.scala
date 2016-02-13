590748d4ffe30a84a67e7a53e7d2a31f741b1184
Vasco Horta <diasfonseca@live.com>
2016-02-13 23:49:59 +0000

    Q22

--- Week6Scala/src/dogYears/DogYears.scala
+++ Week6Scala/src/dogYears/DogYears.scala
@@ -5,4 +5,10 @@ import com.atomicscala.AtomicTest
 object TestDogYears extends App {
   val dogYears = (x: Int) => x * 7
   AtomicTest.any2Atomic(dogYears(10)).is(70)
+  
+  var s = "" 
+  val v = Vector(1, 5, 7, 8) 
+  v.foreach(x => s+=dogYears(x)+" ") 
+  AtomicTest.any2Atomic(s).is("7 35 49 56 ")
+  
 }
