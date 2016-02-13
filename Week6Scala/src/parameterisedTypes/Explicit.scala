c723435ad7ddb409145ff2f3df563138601ac8e5
Vasco Horta <diasfonseca@live.com>
2016-02-13 23:44:12 +0000

    Q20

--- Week6Scala/src/parameterisedTypes/Explicit.scala
+++ Week6Scala/src/parameterisedTypes/Explicit.scala
@@ -4,6 +4,7 @@ import scala.Vector
 import com.atomicscala.AtomicTest
 
 case class ParameterisedTypes() {
+
   // Return type is inferred: 
   def inferred(c1: Char, c2: Char, c3: Char) = { 
     Vector(c1, c2, c3) 
@@ -28,6 +29,14 @@ case class ParameterisedTypes() {
   def explicitSet(v: Vector[Double]) : Set[Double] = {
     v.toSet
   }
+
+  // Explicit return Set
+  def str() : String = {
+    var s : String = ""
+    explicitSet(Vector(1,2,3,4)).foreach{ x => s += x.toInt + "," }
+    s
+  }
+
 }
 
 object testParameterisedTypes extends App {
@@ -36,9 +45,10 @@ object testParameterisedTypes extends App {
   AtomicTest.any2Atomic(ParameterisedTypes().explicitDouble(1.0, 2.0, 3.0)).is(Vector(1.0, 2.0, 3.0))
   AtomicTest.any2Atomic(ParameterisedTypes().explicitList(Vector(10.0, 20.0))).is(List(10.0, 20.0))
   AtomicTest.any2Atomic(ParameterisedTypes().explicitList(Vector(1, 2, 3))).is(List(1.0, 2.0, 3.0))
-
   AtomicTest.any2Atomic(ParameterisedTypes().explicitSet(Vector(10.0, 20.0, 10.0))).is(Set(10.0, 20.0))
   AtomicTest.any2Atomic(ParameterisedTypes().explicitSet(Vector(1, 2, 3, 2, 3, 4))).is(Set(1.0, 2.0, 3.0, 4.0))
 
+  AtomicTest.any2Atomic(ParameterisedTypes().str).is("1,2,3,4,")
+  
 
 }
