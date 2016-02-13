d80f6ddb404b496aba5850625e90d967b58dc7a0
Vasco Horta <diasfonseca@live.com>
2016-02-13 15:14:35 +0000

    Q16

--- Week6Scala/src/caseClasses/CaseClasses.scala
+++ Week6Scala/src/caseClasses/CaseClasses.scala
@@ -2,18 +2,27 @@ package caseClasses
 
 import com.atomicscala.AtomicTest
 
-case class Person(names: String*) {
-  var n = names
-  def first() : String = { n.toList(0) }
-  def last()  : String = { n.toList(1) }
-  def email() : String = { n.toList(2) }
+case class Person(var first : String, var last: String, var email: String) {
+//  def first() : String { return first }
+//  def last()  : String = { last }
+//  def email() : String = { email }
 }
 
 
 
 object caseClasses extends App {
-  val p = Person("Jane", "Smile", "jane@smile.com")
-  AtomicTest.any2Atomic(p.first).is("Jane")
-  AtomicTest.any2Atomic(p.last).is("Smile")
-  AtomicTest.any2Atomic(p.email).is("jane@smile.com")
+//  val p = Person("Jane", "Smile", "jane@smile.com")
+//  AtomicTest.any2Atomic(p.first).is("Jane")
+//  AtomicTest.any2Atomic(p.last).is("Smile")
+//  AtomicTest.any2Atomic(p.email).is("jane@smile.com")
+
+  val people = Vector( 
+      Person("Jane","Smile","jane@smile.com"), 
+      Person("Ron","House","ron@house.com"), 
+      Person("Sally","Dove","sally@dove.com")
+      )
+
+      AtomicTest.any2Atomic(people(0)).is("Person(Jane,Smile,jane@smile.com)")
+      AtomicTest.any2Atomic(people(1)).is("Person(Ron,House,ron@house.com)")
+      AtomicTest.any2Atomic(people(2)).is("Person(Sally,Dove,sally@dove.com)")
 }
