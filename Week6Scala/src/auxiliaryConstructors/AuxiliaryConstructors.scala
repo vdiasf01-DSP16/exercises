23f9b0561d32446a204a39db9de9bb60d9f58eaf
Vasco Horta <diasfonseca@live.com>
2016-02-13 14:33:29 +0000

    Q12 fixed and Q13

--- Week6Scala/src/auxiliaryConstructors/AuxiliaryConstructors.scala
+++ Week6Scala/src/auxiliaryConstructors/AuxiliaryConstructors.scala
@@ -1,6 +1,16 @@
 package auxiliaryConstructors
 
-class ClothesWasher(var modelName : String = "Default", var capacity: Double = 0) {
+class ClothesWasher( var modelName : String, var capacity: Double) {
+  def this() = this("Default", 0)
+  def this(model: String) = this(model, 0)
+  def this(capa: Double) = this("Default", capa)
+
+  override def toString() : String = {
+    "ModelName: " + modelName + ", Capacity: " + capacity
+  }
+}
+
+class ClothesWasher2(var modelName : String = "Default", var capacity: Double = 0) {
   override def toString() : String = {
     "ModelName: " + modelName + ", Capacity: " + capacity
   }
@@ -10,9 +20,19 @@ object AuxiliaryConstructors extends App {
   var cw = new ClothesWasher()
   println(cw.toString())
 
-  var cw1 = new ClothesWasher(modelName = "Something else")
+  var cw1 = new ClothesWasher("Something else")
   println(cw1.toString())
   
-  var cw2 = new ClothesWasher(capacity = 123)
+  var cw2 = new ClothesWasher(123)
   println(cw2.toString())
+
+  
+  var cw3 = new ClothesWasher2()
+  println(cw3.toString())
+
+  var cw4 = new ClothesWasher2(modelName = "Something else")
+  println(cw4.toString())
+  
+  var cw5 = new ClothesWasher2(capacity = 123)
+  println(cw5.toString())
 }
