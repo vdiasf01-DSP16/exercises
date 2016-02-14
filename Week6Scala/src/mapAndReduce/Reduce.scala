633f908208c49164a04693d94f512df93cd475ea
Vasco Horta <diasfonseca@live.com>
2016-02-14 12:53:00 +0000

    Q25&Q26

---
+++ Week6Scala/src/mapAndReduce/Reduce.scala
@@ -0,0 +1,19 @@
+package mapAndReduce
+
+import com.atomicscala.AtomicTest
+
+class Reduce() {
+  def sumIt(list: Int*) : Int = {
+    var result = 0
+    for(value <- list ) {
+      result += value
+    }
+    return result
+  }
+}
+
+object testReduce extends App {
+  var r = new Reduce()
+  AtomicTest.any2Atomic(r.sumIt(1,2,3)).is(6)
+  AtomicTest.any2Atomic(r.sumIt(45,45,45,60)).is(195)
+}
