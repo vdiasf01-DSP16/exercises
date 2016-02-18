f1859208bbf9ccdd7dc1e0ff7fe9a7b0e82ba7bc
Vasco Horta <diasfonseca@live.com>
2016-02-18 19:55:59 +0000

    Q1

--- Week7Scala/src/mapReduce/q1.scala
+++ Week7Scala/src/mapReduce/q1.scala
@@ -8,5 +8,19 @@ object testAnswers extends App {
   val v = Vector(1, 2, 3, 4)
   AtomicTest.any2Atomic(v.map(n => n + 1)).is(Vector(2, 3, 4, 5))
   AtomicTest.any2Atomic(v.map(n => ( n + 1 ) * 10 + n)).is(Vector(21, 32, 43, 54))
+
+  // Foreach does not return the expected result
+  AtomicTest.any2Atomic(v.foreach(n => n + 1)).is(Vector(2, 3, 4, 5))
+  AtomicTest.any2Atomic(v.foreach(n => ( n + 1 ) * 10 + n)).is(Vector(21, 32, 43, 54))
+
+  // For.. mor complicated and prone to errors..
+  var newVector = v
+  var index = 0
+  for(n <- newVector) { 
+    newVector = newVector updated (index, (10 * ( n + 1 ) + n))
+    index += 1
+  }
+  AtomicTest.any2Atomic(newVector).is(Vector(21, 32, 43, 54))
+
 }
 
