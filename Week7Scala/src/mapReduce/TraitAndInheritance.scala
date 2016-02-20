818b5713db74689b89dc8c52296e423a9fbb70eb
Vasco Horta <diasfonseca@live.com>
2016-02-20 12:02:05 +0000

    Q5 c

--- Week7Scala/src/mapReduce/TraitAndInheritance.scala
+++ Week7Scala/src/mapReduce/TraitAndInheritance.scala
@@ -1,6 +1,5 @@
 package mapReduce
 
-
 sealed trait Shape {
   def sides : Int
   def perimeter : Double
@@ -9,10 +8,6 @@ sealed trait Shape {
 
 sealed trait Rectangular extends Shape {
   override def sides() : Int = 4 
-  this match {
-    case Square(size)             => Square(size)
-    case Rectangle(width, height) => Rectangle(width,height)
-  }
 }
 
 final case class Circle(var radius: Double) extends Shape {
@@ -30,22 +25,21 @@ final case class Rectangle(var width: Double, var height: Double) extends Rectan
     override def perimeter() : Double = ( width + height ) * 2
     override def area() : Double = width * height  
 }
-  
+
+object Draw {
+  def apply(shape: Shape) {
+    shape match {
+      case Circle(radius) => println("A circle of radius "+radius+"cm") 
+      case Rectangle(w,h) => println("A rectangle of width "+w+" and height "+h+"cm") 
+      case Square(size)   => println("A square of size "+size+"cm") 
+    }
+  }
+}
+
 object TraitAndInheritance extends App {
 
-  var square = Square(4.5)
-  println(square.sides())
-  println(square.perimeter())
-  println(square.area())
-
-  var circle = Circle(4.5)
-  println(circle.sides())
-  println(circle.perimeter())
-  println(circle.area())  
-  
-  var rectangle = Rectangle(4,5.1)
-  println(rectangle.sides())
-  println(rectangle.perimeter())
-  println(rectangle.area())
+  Draw(Circle(10))
+  Draw(Rectangle(3, 4))
+  Draw(Square(4.5))
 
 }
