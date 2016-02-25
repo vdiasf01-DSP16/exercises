4c7fb9ad74e2eb75b7eee2a3cb49ec1ca2ed3320
Vasco Horta <diasfonseca@live.com>
2016-02-25 20:02:36 +0000

    q1b

--- Week8Scala/src/q1/q1.scala
+++ Week8Scala/src/q1/q1.scala
@@ -1,6 +1,27 @@
 package q1
 
-sealed trait LinkedList[T]
-final case object End extends LinkedList[T]
-final case class Pair(head: Int, tail: LinkedList[T]) extends LinkedList[T]
+import com.atomicscala.AtomicTest
 
+sealed trait LinkedList[T] {
+  def length : Int
+}
+
+final case class End() extends LinkedList[Any] {
+  override val length = 0
+}
+final case class Pair(head: Int, tail: LinkedList[Any]) extends LinkedList[Any] {
+  override def length = {
+    tail match {
+      case End() => 1
+      case _ => 1 + tail.length
+    }
+  }
+}
+
+
+object TestMain extends App {
+  val example = Pair(1, Pair(2, Pair(3, End())))
+  AtomicTest.any2Atomic(example.length).is(3)
+  AtomicTest.any2Atomic(example.tail.length).is(2)
+  AtomicTest.any2Atomic(End().length).is(0)
+}
